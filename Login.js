b7463216db7ffb061d75f8867ceee1dcf031fd8d
Gaetan Pierre <gaetan.pierre19@gmail.com>
2024-10-25 20:12:30 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,25 +1,30 @@
-const loginconnect = ("http://localhost:5678/api/users/login");
+const loginconnect = "http://localhost:5678/api/users/login";
 
-console.log(document.getElementById('login-form'));
 
-document.getElementById('login-form').addEventListener('submit', async function(event) {
-    event.preventDefault(); 
+document.getElementById('login-form').addEventListener('submit', async function (event) {
+  event.preventDefault();
 
-    let user = {
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value,
-    };
-console.log(user)
-    try{
-      let response = await fetch (loginconnect, {
-        method: 'POST',
-       body: user
-})
-    }
-    catch(error)
-    {
-      console.error(error)
-    }
-    
-    
+  let user = {
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value,
+  };
+
+  try {
+    let response = await fetch(loginconnect, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    const result = await response.json()
+
+    console.log(result.token)
+
+  }
+  catch (error) {
+    console.error(error)
+  }
+
 })
